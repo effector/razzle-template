af0a272876e9460879df05a418e45585bdbcd5b9
Sergey Sova <mail@sergeysova.com>
2020-02-15 19:46:22 +0300

    test(application): add fork to test

--- src/application.test.tsx
+++ src/application.test.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { fork } from 'effector/fork';
 
+import { rootDomain } from 'lib/effector';
 import { Application } from './application';
 
 import { MemoryRouter } from 'react-router-dom';
@@ -8,9 +10,10 @@ import { MemoryRouter } from 'react-router-dom';
 describe('<App />', () => {
   test('renders without exploding', () => {
     const div = document.createElement('div');
+    const scope = fork(rootDomain);
     render(
       <MemoryRouter>
-        <Application />
+        <Application root={scope} />
       </MemoryRouter>,
       div,
     );
